e9d2f2709e3aafc93282f0348caaa3ccb510c3ea
Pedro Carvalho <43827142+pedrovrc@users.noreply.github.com>
2018-10-06 17:17:39 -0300

    Diretório base do programa incluído
    Foi feito somente a estrutura básica de diretórios para que o makefile funcione corretamente. Ainda não há código.

---
+++ conta_linhas/testes/CMakeFiles/runTests.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/runTests.dir/testes.cpp.o"
+  "CMakeFiles/runTests.dir/conta_linhas.c.o"
+  "runTests.pdb"
+  "runTests"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/runTests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
