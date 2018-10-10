a5a606f7da35b1a54a752cab93b0989dbe640a8a
Pedro Carvalho <43827142+pedrovrc@users.noreply.github.com>
2018-10-09 21:58:54 -0300

    Mais casos de teste geral

--- conta_linhas/testes/testes.cpp
+++ conta_linhas/testes/testes.cpp
@@ -7,6 +7,18 @@ TEST(conta_linhas_codigo, teste_geral_1) {
 	//contar todas as linhas
 }
 
+TEST (conta_linhas_codigo, teste_geral_2) {
+	FILE* codigo = fopen("amostra2.cpp", "r");
+	ASSERT_EQ(27, conta_linhas_codigo(codigo));
+	//contar todas as linhas
+}
+
+TEST (conta_linhas_codigo, teste_geral_3) {
+	FILE* codigo = fopen("amostra3.cpp", "r");
+	ASSERT_EQ(16, conta_linhas_codigo(codigo));
+	//contar todas as linhas
+}
+
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
