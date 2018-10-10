923076cc0ffa57b2f9f9bf7b7f246e2411dde9a5
Pedro Carvalho <43827142+pedrovrc@users.noreply.github.com>
2018-10-09 22:27:53 -0300

    Arquivos p/ testes

--- conta_linhas/testes/testes.cpp
+++ conta_linhas/testes/testes.cpp
@@ -1,6 +1,40 @@
 #include "conta_linhas.c"
 #include <gtest/gtest.h>
 
+TEST (checa_vazia, linha_vazia) {
+	FILE* texto = fopen("linha_vazia.txt", "r");
+	char linha[25];
+
+	fgets(linha, 25, texto);
+	ASSERT_EQ(1, checa_vazia(linha, 25));
+
+	fclose(texto);
+}
+
+TEST (checa_vazia, linha_codigo) {
+	FILE* texto = fopen("linha_codigo.txt", "r");
+	char linha[25];
+
+	fgets(linha, 25, texto);
+	ASSERT_EQ(0, checa_vazia(linha, 25));
+
+	fclose(texto);
+}
+
+TEST (checa_vazia, linha_comentario) {
+	FILE* texto = fopen("linha_comentario.txt", "r");
+	char linha[35];
+	
+	fgets(linha, 35, texto);
+	ASSERT_EQ(0, checa_vazia(linha, 35));
+
+	fgets(linha, 35, texto);
+	fgets(linha, 35, texto);
+	ASSERT_EQ(0, checa_vazia(linha, 35));
+
+	fclose(texto);
+}
+
 TEST(conta_linhas_codigo, teste_geral_1) {
 	FILE* codigo = fopen("amostra1.cpp", "r");
 	ASSERT_EQ(28, conta_linhas_codigo(codigo));
